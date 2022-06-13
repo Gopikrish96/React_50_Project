e53858b9eb48b0ce7d431f460acae1228e851f10
Gopi <gopikrishnan.s@greatinnovus.com>
2022-06-13 18:53:38 +0530

    Header Updated

--- src/1_Eshop/header.js
+++ src/1_Eshop/header.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SearchIcon from '@material-ui/icons/Search';
+import './Header.css'
 
 function Header() {
   return (
-    <div className="eader">
-        <div className="header__icon"></div>
-        <div className="header__search"></div>
+    <div className="header">
+        <div className="header__logo">
+            <StorefrontIcon fontSize='large' className='header__logoimage'></StorefrontIcon>
+            <h2 className="header__logotitle">eSHOP</h2>
+
+        </div>
+        <div className="header__search">
+            <input type="text" placeholder='Search' className='header__searchInput'/>
+            <SearchIcon fontsize="large" className='header__searchIcon'></SearchIcon>
+        </div>
         <div className="header__nav">
             <div className="nav__item">
                 <span className="nav__itemLineOne">Hello Guest</span>
@@ -15,8 +26,8 @@ function Header() {
                 <span className="nav__itemLineTwo">Shop</span>
             </div>
             <div className="nav__item">
-                <span className="nav__itemLineOne">Basket</span>
-                <span className="nav__itemLineTwo">0</span>
+                <ShoppingBasketIcon fontsize="large"></ShoppingBasketIcon>
+                <span className="nav__itemLineTwo nav__basketCount">0</span>
             </div>
         </div>
     </div>
