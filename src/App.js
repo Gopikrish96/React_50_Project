849992145f9762615ed24ac0615846d21c9b337d
Gopi <gopikrishnan2409@gmail.com>
2022-06-13 21:56:46 +0530

    APPjs Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Header from "./1_Eshop/header";
+import Header from "./1_Eshop/Header";
+import Home from './1_Eshop/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home></Home>
        
     </div>
   );
